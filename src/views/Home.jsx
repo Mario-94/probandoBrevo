27e75a2a715946e13f7bccb09ee6f1d541f7982b
Mario <mags9415@gmail.com>
2024-07-25 14:04:13 -0600

    instalación y configuración de boostrap creamos un pequeño formulario para aplicar lo de envio de archivos

---
+++ src/views/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import  Formulario  from '../components/Formulario'
+
+
+export const Home = () => {
+  return (
+  <>
+  <h1>Formulario brevo</h1>
+    <Formulario/>
+  </>
+  )
+}
